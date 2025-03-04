8cf730104b946ececede729c57c99d5929d18781
Ben <benwilliamscomapi@outlook.com>
2025-03-04 11:47:02 +0000

    Add js function for mobile navbar

--- javascript/script.js
+++ javascript/script.js
@@ -1,1 +1,11 @@
-console.log('Hiya')
+// Toggle mobile menu
+
+
+document.addEventListener('DOMContentLoaded', () => {
+    const hamburgerButton = document.querySelector('.hamburger-button');
+    const mobileMenu = document.querySelector('.mobile-menu');
+
+    hamburgerButton.addEventListener('click', () => mobileMenu.classList.toggle('active')
+);
+
+});
