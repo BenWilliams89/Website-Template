bc3e1dab3236d787b7b80ec119743b4c04a8ff5e
Ben <benwilliamscomapi@outlook.com>
2025-03-06 14:16:39 +0000

    Add Javacsript function to FAQ section

--- javascript/script.js
+++ javascript/script.js
@@ -1,6 +1,5 @@
 // Toggle mobile menu
 
-
 document.addEventListener('DOMContentLoaded', () => {
     const hamburgerButton = document.querySelector('.hamburger-button');
     const mobileMenu = document.querySelector('.mobile-menu');
@@ -9,3 +8,37 @@ document.addEventListener('DOMContentLoaded', () => {
 );
 
 });
+
+
+// Function for FAQ Opening and closing body content
+
+
+// Select all the icons in the FAQ headers
+const faqHeaders = document.querySelectorAll('.faq-group-header i');
+
+// Add click event listener to each icon
+faqHeaders.forEach(function(icon){
+    icon.addEventListener('click', function(){
+        const faqGroupBody = icon.parentElement.nextElementSibling;
+
+        // Close any open FAQ bodies
+        const openFaqBodies = document.querySelectorAll('.faq-group-body.open')
+        openFaqBodies.forEach(function(openBody){
+            if (openBody !== faqGroupBody) {
+                openBody.classList.remove('open');
+                const openIcon = openBody.previousElementSibling.querySelector('i');
+                openIcon.classList.remove('fa-minus');
+                openIcon.classList.add('fa-plus')
+            }
+        });
+        
+        // Toggle the 'open' class on the clicked FAQ body
+
+        faqGroupBody.classList.toggle('open');
+
+        // Toggle the icon between plus and minus
+        
+        icon.classList.toggle('fa-plus');
+        icon.classList.toggle('fa-minus')
+    })
+})
